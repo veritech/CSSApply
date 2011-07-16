c9537afe35deb7aec37721fa0b93c9897fd1506c
Jonathan Dalrymple <jonathan@float-right.co.uk>
2011-07-16 14:52:12 -0700

    Added the GHUnit lib

---
+++ CSSSampleTests/CSSUIViewTest.m
@@ -0,0 +1,33 @@
+//
+//  CSSUIViewTest.m
+//  CSSSample
+//
+//  Created by Jonathan Dalrymple on 16/07/2011.
+//  Copyright 2011 Float:Right Ltd. All rights reserved.
+//
+
+#import "CSSUIViewTest.h"
+
+
+@implementation CSSUIViewTest
+
+#if USE_APPLICATION_UNIT_TEST     // all code under test is in the iPhone Application
+
+- (void)testAppDelegate {
+    
+    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+    
+}
+
+#else                           // all code under test must be linked into the Unit Test bundle
+
+- (void)testMath {
+    
+    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
+    
+}
+
+#endif
+
+@end
