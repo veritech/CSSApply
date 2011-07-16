a2bfeca3e31ea7aa2c11839ac4eae58dd89a0564
Jonathan Dalrymple <jonathan@float-right.co.uk>
2011-07-16 13:02:39 -0700

    Created the UIVew category

---
+++ UIVIew+CSS.h
@@ -0,0 +1,14 @@
+//
+//  UIVIew+CSS.h
+//  CSSSample
+//
+//  Created by Jonathan Dalrymple on 16/07/2011.
+//  Copyright 2011 Float:Right Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIView (UIVIew_CSS)
+
+@end
---
+++ UIVIew+CSS.m
@@ -0,0 +1,14 @@
+//
+//  UIVIew+CSS.m
+//  CSSSample
+//
+//  Created by Jonathan Dalrymple on 16/07/2011.
+//  Copyright 2011 Float:Right Ltd. All rights reserved.
+//
+
+#import "UIVIew+CSS.h"
+
+
+@implementation UIView (UIVIew_CSS)
+
+@end
